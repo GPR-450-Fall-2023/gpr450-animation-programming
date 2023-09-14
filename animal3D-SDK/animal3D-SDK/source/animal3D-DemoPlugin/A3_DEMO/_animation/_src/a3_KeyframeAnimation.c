46344ec75c9bf458341e6278536253251995846f
Joseph Lyons <joseph.lyons@mymail.champlain.edu>
2023-09-14 11:03:36 -0400

    Added some comments to a3_KeyframeAnimation

--- animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_src/a3_KeyframeAnimation.c
+++ animal3D-SDK/animal3D-SDK/source/animal3D-DemoPlugin/A3_DEMO/_animation/_src/a3_KeyframeAnimation.c
@@ -60,10 +60,9 @@ a3i32 a3keyframePoolCreate(a3_KeyframePool* keyframePool_out, const a3ui32 count
 	const a3real KEYFRAME_DEFAULT_DURATION = 1; // init each keyframe with this duration
 	const a3ui32 KEYFRAME_DEFAULT_DATA = 0; // init each keyframe with this value
 
-	//keyframePool_out = (a3_KeyframePool*) malloc(sizeof(a3_KeyframePool));
-
 	if (keyframePool_out == NULL) return -1;
 
+	// allocate keyframe array
 	keyframePool_out->keyframe = (a3_Keyframe*) malloc(count * sizeof(a3_Keyframe));
 
 	if(keyframePool_out->keyframe == NULL) return -1;
@@ -72,6 +71,7 @@ a3i32 a3keyframePoolCreate(a3_KeyframePool* keyframePool_out, const a3ui32 count
 
 	for (a3ui16 i = 0; i < count; i++)
 	{
+		// init each keyframe with default values
 		a3keyframeInit((keyframePool_out->keyframe + i), KEYFRAME_DEFAULT_DURATION, KEYFRAME_DEFAULT_DATA);
 		(keyframePool_out->keyframe + i)->index = i; //Log keyframes index in pool
 	}
@@ -86,6 +86,7 @@ a3i32 a3keyframePoolRelease(a3_KeyframePool* keyframePool)
 	
 	int returnCode = 0;
 	
+	// release keyframe array if it exists
 	if (keyframePool->keyframe == NULL)
 	{
 		returnCode = 1;
@@ -106,7 +107,7 @@ a3i32 a3keyframeInit(a3_Keyframe* keyframe_out, const a3real duration, const a3u
 	if(keyframe_out == NULL) return -1;
 
 	keyframe_out->data = value_x;
-	keyframe_out->setDuration = a3keyframeSetDuration;
+	keyframe_out->setDuration = a3keyframeSetDuration; // give keyframe appropriate setter function
 	keyframe_out->setDuration(keyframe_out, duration);
 
 	return 0;
@@ -118,18 +119,18 @@ a3i32 a3clipPoolCreate(a3_ClipPool* clipPool_out, a3_KeyframePool* keyframePool,
 {
 	const a3byte DEFAULT_CLIP_NAME[] = "Clip";
 	const a3ui32 DEFAULT_FIRST_INDEX = 0;
-	a3ui32 lastIndex = DEFAULT_FIRST_INDEX + keyframePool->count - 1; //Default last index should be the index of the last keyframe in the pool
 
-	//clipPool_out = (a3_ClipPool*) malloc(sizeof(a3_ClipPool));
+	a3ui32 lastIndex = DEFAULT_FIRST_INDEX + keyframePool->count - 1; //Default last index should be the index of the last keyframe in the pool
 
 	if(clipPool_out == NULL) return -1;
 
 	clipPool_out->count = count;
 
-	clipPool_out->clip = (a3_Clip*) malloc(sizeof(a3_Clip) * count);
+	clipPool_out->clip = (a3_Clip*) malloc(sizeof(a3_Clip) * count); // create clip array
 
 	for (a3ui32 i = 0; i < count; i++)
 	{
+		// init each clip with default values
 		a3clipInit((clipPool_out->clip + i), DEFAULT_CLIP_NAME, keyframePool, DEFAULT_FIRST_INDEX, lastIndex);
 		
 		if ((clipPool_out->clip + i) == NULL) return -1;
@@ -149,7 +150,7 @@ a3i32 a3clipPoolRelease(a3_ClipPool* clipPool)
 
 	int returnCode = 0;
 
-	if (clipPool->clip == NULL)
+	if (clipPool->clip == NULL) // only free clip array if it exists
 	{
 		returnCode = 1;
 	}
@@ -168,7 +169,7 @@ a3i32 a3clipInit(a3_Clip* clip_out, const a3byte clipName[a3keyframeAnimation_na
 {
 	if(clip_out == NULL) return -1;
 
-	// clip_out->name = clipName;
+	// copy passed in name to clip name
 	strcpy(clip_out->name, clipName);
 
 	clip_out->keyframePool = keyframePool;
@@ -190,7 +191,7 @@ a3i32 a3clipGetIndexInPool(const a3_ClipPool* clipPool, const a3byte clipName[a3
 
 	for (a3ui32 i = 0; i < clipPool->count; i++)
 	{
-		if (strcmp(clipPool->clip[i].name, clipName) == 0)
+		if (strcmp(clipPool->clip[i].name, clipName) == 0) // true if we've found the name we're looking for
 		{
 			return i;
 		}
